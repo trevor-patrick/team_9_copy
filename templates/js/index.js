5d274fab0dfc14a620ef40dfd94078cbde91e8ff
anoopbabu29 <anoop.bab@gmail.com>
2018-10-05 23:41:25 -0400

    Slight change

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,7 +1,17 @@
+var ;
+
 $(document).ready(function(){
     $('#logBtn').click(function(){
         $('html, body').animate({
             scrollTop: $('#LogInPart').offset().top
         }, 750);
     });
+    if ('speechSynthesis' in window) {
+        /* speech synthesis supported */
+        console.log('yes');
+    }
+    else {
+        /* speech synthesis not supported */
+        console.log('no');
+    }
 });
