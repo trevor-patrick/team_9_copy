292d1362d8a0e9f06645e34cec1d8dea2bb98ea2
trevor.patrick <40744678+trevor-patrick@users.noreply.github.com>
2018-10-05 22:46:26 -0400

    backend logic stub

--- app.py
+++ app.py
@@ -7,13 +7,32 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# engine = create_engine('postgres://olxpdkjidxzijo:6c0ff6d277bd117d6e615144dab8b3fe26e190e0788c30c9aa4c6de74076af2d@ec2-54-235-242-63.compute-1.amazonaws.com:5432/d25m2jqruo7e1o')
-# db = scoped_session(sessionmaker(bind=engine))
+engine = create_engine('postgres://olxpdkjidxzijo:6c0ff6d277bd117d6e615144dab8b3fe26e190e0788c30c9aa4c6de74076af2d@ec2-54-235-242-63.compute-1.amazonaws.com:5432/d25m2jqruo7e1o')
+db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/", methods=['POST', 'GET'])
+email = None
+
+@app.route("/", methods=['GET'])
 def index():
-    if request.method == "GET":
-        return render_template("index.html", username_form = username_form)
-    #
-    # username request.form.get("Username")
-    # return render_template("index.html", )
+    return render_template("index.html")
+
+@app.route("/", methods=['POST'])
+def serve_form():
+    email = request.form.get("Username")
+    #these inputs are sanitized
+    check = db.execute("SELECT * FROM users WHERE email == :email", {"email": email}).fetchone()
+    if not check == None:                           # user exists
+        return render_template('password.html')
+    else:                                           # user doesn't exist
+        return render_template('register.html')
+
+@app.route("/", methods=['POST'])
+def serve_dashboardteam-9_existing_user():
+    password = request.form.get("Password")
+    #TODO validate password (length, characters, etc). Also, hash
+    #TODO make sure password is correct from database
+
+    return render_template('dashboard.html')
+
+@app.route("/", methods=['POST'])
+def serve_dashboard_new_user():
