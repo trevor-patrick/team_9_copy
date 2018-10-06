fb0c31522dc40632fab1591ab223dbfc008ea7c7
anoopbabu29 <anoop.bab@gmail.com>
2018-10-06 06:36:04 -0400

    Big commit for login backend

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import os
 import sys
 import hashlib
+import mysql.connector
 
 from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="chicken77",
+  database="hackathon"
+)
+
+email = ""
+
+myCursor = mydb.cursor(prepared=True)
+
 app = Flask(__name__)
 
-# engine = create_engine('')
-# db = scoped_session(sessionmaker(bind=engine))
 
-correct_email = 'test_email'
-correct_password = hashlib.md5('testpass'.encode()).hexdigest()
-
-email = None
 
 @app.route("/", methods=['GET'])
 def index():
@@ -23,20 +30,40 @@ def index():
 @app.route("/", methods=['POST'])
 def serve_form():
     email = request.form.get("email")
+    myCursor.execute("UPDATE tempemail set Email = %s", (email,))
+    mydb.commit()
+
+
     #these inputs are sanitized
-    #check = db.execute("SELECT * FROM users WHERE email == :email", {"email": email}).fetchone()
-    if email == correct_email:
-        first_name = "Trevor"
+    myCursor.execute("SELECT COUNT(*) FROM Person WHERE Email = %s", (email,))
+    myresult = myCursor.fetchall()
+
+    if myresult[0][0]:          #account found
+        myCursor.execute("SELECT FirstName FROM Person WHERE Email = %s", (email,))
+        myresult = myCursor.fetchone()
+        first_name = str(myresult[0].decode())
         display_text = "Hi, " + first_name + ". We're glad to have you back!"
+
         return render_template('password.html', display_text = display_text)
     else:                                           # user doesn't exist
         return render_template('register.html')
 
-@app.route("/#pass", methods=['POST'])
+@app.route("/pass", methods=['POST'])
 def serve_dashboard_existing_user():
     password = request.form.get("password")
-    #TODO make sure password is correct from database
-    if hashlib.md5(password.encode()).hexdigest() == correct_password: #select password where email = email, compare to correct_password
+
+    myCursor.execute("SELECT Email FROM tempemail")
+    myresult = myCursor.fetchall()
+    db_email = str(myresult[0][0].decode())
+
+    myCursor.execute("SELECT Password FROM Person WHERE Email = %s", (db_email,))
+    myresult = myCursor.fetchone()
+    # print("our info: " + myresult[0].decode())
+    # passwordHash = hashlib.md5(str(myresult[0].decode()).encode()).hexdigest()
+
+    print("[[_ " + db_email + " <------username")
+    print(myresult[0].decode() + " <-------password")
+    if hashlib.md5(password.encode()).hexdigest() == myresult[0].decode():
         return render_template('dashboard.html')
     else:
         display_text = "Incorrect credentials!"
